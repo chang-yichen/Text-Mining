99e71475522a7cfaca3c5bcd055b9165331a3062
Chang Yi Chen <changyichen@u.nus.edu>
2023-05-28 22:31:55 +0800

    link submit and next functions

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from flask import Flask, request
 import requests
 from config import TOKEN
 import json
+import io, os, sys, types
+from chatbot_functions import submitting_and_next
 
 app = Flask(__name__)
 
-user_filters = {}
+# user_filters = {}
+filter_lst = []
 
 @app.route('/', methods=['POST'])
 def telegram_webhook():
     data = request.get_json()
+    count = 1
     chat_id = data['message']['chat']['id']
     message_text = data['message']['text']
     
@@ -17,49 +21,50 @@ def telegram_webhook():
         send_message(chat_id, 'Welcome to the Chatbot! Please provide the following filters:')
         send_message(chat_id, 'Price (Enter a value):')
     elif 'Price' in message_text:
-        user_filters[chat_id] = {'Price': message_text}
-        send_message(chat_id, 'Service (Enter a value):')
-    elif 'Service' in message_text:
-        user_filters[chat_id]['Service'] = message_text
-        send_message(chat_id, 'Environment (Enter a value):')
-    elif 'Environment' in message_text:
-        user_filters[chat_id]['Environment'] = message_text
+        # user_filters[chat_id] = {'Price': message_text}
+        filter_lst.append(message_text[6:])
         send_message(chat_id, 'Quality (Enter a value):')
     elif 'Quality' in message_text:
-        user_filters[chat_id]['Quality'] = message_text
+        # user_filters[chat_id] = {'Price': message_text}
+        filter_lst.append(message_text[8:])
+        send_message(chat_id, 'Environment (Enter a value):')
+    # elif 'Service' in message_text:
+    #     # user_filters[chat_id]['Service'] = message_text
+    #     filter_lst.append(message_text)
+    #     send_message(chat_id, 'Environment (Enter a value):')
+    elif 'Environment' in message_text:
+        # user_filters[chat_id]['Environment'] = message_text
+        filter_lst.append(message_text[12:])
+        send_message(chat_id, 'Food Type (Enter a value):')
+    elif 'Food Type' in message_text:
+        # user_filters[chat_id]['Quality'] = message_text
+        filter_lst.append(message_text[10:])
         send_message(chat_id, 'Thank you for providing the filters!')
         process_filters(chat_id)
+        send_message(chat_id,f"Type 'Next' to get an alternate recommendation~")
+    elif 'Next' in message_text:
+        next_recomendation(chat_id, count)
     else:
         send_message(chat_id, 'Invalid input, please try again.')
     
     return 'OK'
 
 def process_filters(chat_id):
-    filters = user_filters.get(chat_id)
-    restaurant = rank_restaurants(filters)
-    if restaurant:
-        send_message(chat_id, f"Restaurant matching the filters: {restaurant}")
+    output_dic = submitting_and_next(filter_lst)
+    if len(output_dic) != 0:
+        send_message(chat_id, f"Recommended restaurant: {output_dic['Restaurant']}")
+        send_message(chat_id, f"Summary: {output_dic['Summary']}")
     else:
         send_message(chat_id, "No restaurant found matching the filters.")
 
-def rank_restaurants(filters):
-    # Implement your restaurant ranking logic using the provided filters
-    # Return the restaurant that matches the filters
-    # Replace the code below with your ranking logic
-    price_filter = filters.get('Price')
-    service_filter = filters.get('Service')
-    environment_filter = filters.get('Environment')
-    quality_filter = filters.get('Quality')
-
-    # Placeholder code to demonstrate the output
-    if price_filter == 'cheap' and service_filter == 'excellent':
-        return 'Restaurant A'
-    elif price_filter == 'moderate' and service_filter == 'good':
-        return 'Restaurant B'
-    elif price_filter == 'expensive' and service_filter == 'poor':
-        return 'Restaurant C'
+def next_recomendation(chat_id, count):
+    output_dic = submitting_and_next(filter_lst, count)
+    count += 1
+    if len(output_dic) != 0:
+        send_message(chat_id, f"Recommended restaurant: {output_dic['Restaurant']}")
+        send_message(chat_id, f"Summary: {output_dic['Summary']}")
     else:
-        return None
+        send_message(chat_id, "No restaurant found matching the filters.")
 
 def send_message(chat_id, text):
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
---
+++ chatbot_functions.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import csv
+
+df = pd.read_csv("small_df.csv",delimiter=',')
+
+feature_df = pd.read_csv("tfidf_matrix.csv",delimiter=',')
+feature_df.drop(columns=['Unnamed: 0'], inplace=True)
+
+with open("Summaries_of_restaurants.csv", "r", newline="") as csvfile:
+  reader = csv.reader(csvfile)
+  summary = {}
+  summ = []
+  for row in reader:
+    summ.append(row[1])
+  i=1
+  for key in df:
+    summary[key] = summ[i]
+    i = i+1
+
+def initilizing():
+    corpus = []#all possible valid input words 
+    for key in feature_df:
+        corpus.append(key)
+
+    return corpus
+
+#rank all restaurants, get their scores, which store in 'ranks'
+#input:a list of string(words from all filters)
+#output:a dictionary, value is the score of restaurant
+def ranking(filter_words, corpus):
+    ranks = {}
+    for key in df:
+        ranks[key] = 0
+    for word in filter_words: 
+        if(word in corpus): 
+            n=0
+            i=0
+            for key in ranks:
+                ranks[key] = (n*ranks[key]+feature_df[word][i])/(n+1)
+                i=i+1
+            n = n+1
+    return ranks
+
+
+#used by submit and next button
+#input:next_time is default when first time called , and a list of four filter words
+#next_time is the time that next button is pressed
+#output:a string(name of a restaurant)
+def submitting_and_next(filter_words, next_time=0):
+    corpus = initilizing()
+    ranks = ranking(filter_words, corpus)
+    #pick the (next_times+1)th best restaurant
+
+    for i in range(next_time+1):
+        recommend = "十六區和風料理"
+        highest_score = ranks["十六區和風料理"]
+        for key in ranks:
+            if (ranks[key] > highest_score):
+                recommend = key
+                highest_score = ranks[key]
+        ranks[recommend] = 0
+
+    if(highest_score == 0):
+        return {"Restaurant":"No other recommendation","Summary":"NULL"}
+    return {"Restaurant":recommend,"Summary":summary[recommend]}
