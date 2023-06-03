d1c4854ad5e3f66e301e0efc334c34bcffc1f8b2
Chang Yi Chen <changyichen@u.nus.edu>
2023-06-03 11:32:07 +0800

    add url and tidy up phrasing

--- app.py
+++ app.py
@@ -19,30 +19,31 @@ def telegram_webhook():
     
     if message_text == '/start':
         send_message(chat_id, 'Welcome to the Chatbot! Please provide the following filters:')
-        send_message(chat_id, 'Price (Enter a value):')
-    elif 'Price' in message_text:
+        send_message(chat_id, 'Enter the filter name and then the filter value (etc price affordable)')
+        send_message(chat_id, 'price (etc affordable, average, expensive):')
+    elif 'price' in message_text:
         # user_filters[chat_id] = {'Price': message_text}
         filter_lst.append(message_text[6:])
-        send_message(chat_id, 'Quality (Enter a value):')
-    elif 'Quality' in message_text:
+        send_message(chat_id, 'quality (etc delicious, fresh, fast, favourite):')
+    elif 'quality' in message_text:
         # user_filters[chat_id] = {'Price': message_text}
         filter_lst.append(message_text[8:])
-        send_message(chat_id, 'Environment (Enter a value):')
+        send_message(chat_id, 'environment (etc friendly, available, local):')
     # elif 'Service' in message_text:
     #     # user_filters[chat_id]['Service'] = message_text
     #     filter_lst.append(message_text)
     #     send_message(chat_id, 'Environment (Enter a value):')
-    elif 'Environment' in message_text:
+    elif 'environment' in message_text:
         # user_filters[chat_id]['Environment'] = message_text
         filter_lst.append(message_text[12:])
-        send_message(chat_id, 'Food Type (Enter a value):')
-    elif 'Food Type' in message_text:
+        send_message(chat_id, 'food type (etc beef, chicken, dessert):')
+    elif 'food type' in message_text:
         # user_filters[chat_id]['Quality'] = message_text
         filter_lst.append(message_text[10:])
         send_message(chat_id, 'Thank you for providing the filters!')
         process_filters(chat_id)
-        send_message(chat_id,f"Type 'Next' to get an alternate recommendation~")
-    elif 'Next' in message_text:
+        send_message(chat_id,f"Type 'next' to get an alternate recommendation~")
+    elif 'next' in message_text:
         next_recomendation(chat_id, count)
     else:
         send_message(chat_id, 'Invalid input, please try again.')
@@ -54,6 +55,7 @@ def process_filters(chat_id):
     if len(output_dic) != 0:
         send_message(chat_id, f"Recommended restaurant: {output_dic['Restaurant']}")
         send_message(chat_id, f"Summary: {output_dic['Summary']}")
+        send_message(chat_id, f"Google maps URL: {output_dic['URL']}")
     else:
         send_message(chat_id, "No restaurant found matching the filters.")
 
@@ -63,6 +65,7 @@ def next_recomendation(chat_id, count):
     if len(output_dic) != 0:
         send_message(chat_id, f"Recommended restaurant: {output_dic['Restaurant']}")
         send_message(chat_id, f"Summary: {output_dic['Summary']}")
+        send_message(chat_id, f"Google maps URL: {output_dic['URL']}")
     else:
         send_message(chat_id, "No restaurant found matching the filters.")
 
--- chatbot_functions.py
+++ chatbot_functions.py
@@ -6,6 +6,12 @@ df = pd.read_csv("small_df.csv",delimiter=',')
 feature_df = pd.read_csv("tfidf_matrix.csv",delimiter=',')
 feature_df.drop(columns=['Unnamed: 0'], inplace=True)
 
+# load the URL data
+url_dict = pd.read_csv("restaurant_url.csv", header=None, index_col=0)
+url_dict = url_dict[1].to_dict()
+# delete the header
+del url_dict['Restaurant']
+
 with open("Summaries_of_restaurants.csv", "r", newline="") as csvfile:
   reader = csv.reader(csvfile)
   summary = {}
@@ -46,11 +52,10 @@ def ranking(filter_words, corpus):
 #input:next_time is default when first time called , and a list of four filter words
 #next_time is the time that next button is pressed
 #output:a string(name of a restaurant)
-def submitting_and_next(filter_words, next_time=0):
+def submitting_and_next(filter_words=[], next_time=0):
     corpus = initilizing()
     ranks = ranking(filter_words, corpus)
     #pick the (next_times+1)th best restaurant
-
     for i in range(next_time+1):
         recommend = "十六區和風料理"
         highest_score = ranks["十六區和風料理"]
@@ -62,4 +67,4 @@ def submitting_and_next(filter_words, next_time=0):
 
     if(highest_score == 0):
         return {"Restaurant":"No other recommendation","Summary":"NULL"}
-    return {"Restaurant":recommend,"Summary":summary[recommend]}
+    return {"Restaurant":recommend,"Summary":summary[recommend], "URL":url_dict[recommend]}
